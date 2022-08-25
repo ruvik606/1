c2bcaf60500a8684ae96e863857052ad09360c10
ruvik606 <ruvik606@gmail.com>
2022-08-25 09:25:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use stcrict"
+
+console.log("arr" + " - object");
+console.log(4 +  "5") // унарный плюс + + 
+
+let incr = 10,
+    decr = 10;
+/*     
+
+incr++; // инкромент увелечиния на 1
+decr--; // уменшить на 1 
+
+++incr; // префиксные
+--decr; // префексные */ 
+
+console.log(incr++);
+console.log(decr--); // если сразу взяли то постфиксная вертается в 10, сначала старая
+
+console.log(10%2);
+
+console.log(2 + 2 * 2 != '8');
+
+const isChecked = true;
+      isClose = false;
+
+console.log(isChecked || !isClose);
